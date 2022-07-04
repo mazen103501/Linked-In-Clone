53cfee34da28da809d1491ae44d6a12b3e8f365e
mazen103501 <74799603+mazen103501@users.noreply.github.com>
2022-07-04 23:06:43 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,22 +2,4 @@
 // import { initializeApp } from 'firebase/app';
 // import { getFirestore } from "firebase/firestore";
 
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC9qdihaAqnAF16fTA9ghv9K-QT5pgFA4Q",
-  authDomain: "linkedin-clone-eebae.firebaseapp.com",
-  projectId: "linkedin-clone-eebae",
-  storageBucket: "linkedin-clone-eebae.appspot.com",
-  messagingSenderId: "935120846206",
-  appId: "1:935120846206:web:5cd5bcce7f48dddcc5872f",
-};
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-
-export { db, auth };
+// firebase config
